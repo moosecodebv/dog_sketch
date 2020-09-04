189fd0b180d1dccd6b7ee0c23b2b3fbcc2bf86ee
Derek Kraan <derek.kraan@gmail.com>
2020-09-04 13:01:37 +0200

    Add package information and publish to hex.pm

--- mix.exs
+++ mix.exs
@@ -4,9 +4,12 @@ defmodule DogSketch.MixProject do
   def project do
     [
       app: :dog_sketch,
+      name: "DogSketch",
+      package: package(),
       version: "0.1.0",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
+      source_url: "https://github.com/moosecodebv/dog_sketch",
       deps: deps()
     ]
   end
@@ -23,6 +26,15 @@ defmodule DogSketch.MixProject do
     [
       {:propcheck, "~> 1.2", only: :test},
       {:benchee, "~> 1.0", only: :dev}
+    ]
+  end
+
+  defp package do
+    [
+      description: "DDSketch helps you make fast, low-memory, fully mergeable quantile sketches",
+      licenses: ["MIT"],
+      links: %{GitHub: "https://github.com/moosecodebv/dog_sketch"},
+      maintainers: ["Derek Kraan"]
     ]
   end
 end
